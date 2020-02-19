1c9330d93219daed73253763e89a7843a9245473
me <dabeckham@ucdavis.edu>
2020-02-18 19:24:33 -0800

    Wrap up authentication and add login/logout/password forms and templates

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.core.urlresolvers import reverse
-from django.urls import resolve
+from django.urls import reverse, resolve
 from django.test import TestCase
 
 from ..views import signup
